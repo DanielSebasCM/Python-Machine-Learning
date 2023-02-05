243edf52ffdb7bc7b4d9f9e870f0017dc2fce66b
DanielSebasCM <danielsebascm@hotmail.com>
2023-02-05 13:59:06 -0500

    Add snake vision

--- snake_ml/Snake.py
+++ snake_ml/Snake.py
@@ -1,4 +1,11 @@
+from Food import Food
+import random
+
+
 class Snake:
+
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1),
+                  (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
     def __init__(self, grid: tuple[int, int], size: int = 3, speed: float = 3) -> None:
         self.size = size
@@ -7,7 +14,42 @@ class Snake:
         self.direction = [1, 0]
         self.speed = speed
         self.grid = grid
+        self.food = self.getRandFood()
         pass
+
+    def move(self):
+
+        # Move the head
+        self.head = (self.head[0] + self.direction[0],
+                     self.head[1] + self.direction[1])
+
+        # Check if the snake ate food
+        if self.head == self.food.pos:
+            self.size += 1
+            self.food = self.getRandFood()
+
+        # Check if the snake is too long
+        if (len(self.body) > self.size):
+            self.body.pop()
+
+        # Check if the snake went out of bounds
+        if not (0 <= self.head[0] < self.grid[0] and 0 <= self.head[1] < self.grid[1]):
+            raise Exception("Snake went out of bounds")
+
+        # Check if the snake ate itself
+        if (self.contains(self.head)):
+            raise Exception("Snake ate itself")
+
+        # Move the body
+        self.body.insert(0, self.head)
+
+    def getRandFood(self):
+        x = random.randint(0, self.grid[0] - 1)
+        y = random.randint(0, self.grid[1] - 1)
+        if self.contains((x, y)):
+            return self.getRandFood()
+
+        return Food(x, y)
 
     def contains(self, point) -> bool:
         for body_part in self.body:
@@ -15,21 +57,29 @@ class Snake:
                 return True
         return False
 
-    def move(self):
+    def look(self):
 
-        new_head = (self.head[0] + self.direction[0],
-                    self.head[1] + self.direction[1])
-        self.head = new_head
+        vision = []
 
-        if (self.contains(new_head)):
-            raise Exception("Snake ate itself")
+        for direction in self.directions:
+            pos = self.head[0] + direction[0], self.head[1] + direction[1]
+            distance = 1
+            food_dist = 0
+            snake_dist = 0
+            wall_dist = 0
+            while (0 <= pos[0] < self.grid[0] and 0 <= pos[1] < self.grid[1]):
+                if not food_dist and self.food.pos == pos:
+                    food_dist = distance
+                if not snake_dist and self.contains(pos):
+                    snake_dist = distance
 
-        if not (0 <= new_head[0] < self.grid[0] and 0 <= new_head[1] < self.grid[1]):
-            raise Exception("Snake went out of bounds")
+                pos = pos[0] + direction[0], pos[1] + direction[1]
+                distance += 1
 
-        self.body.insert(0, new_head)
+            wall_dist = distance
 
-        if (len(self.body) > self.size):
-            self.body.pop()
+            vision.append(food_dist)
+            vision.append(snake_dist)
+            vision.append(wall_dist)
 
-        pass
+        return vision
--- snake_ml/game.py
+++ snake_ml/game.py
@@ -1,142 +1,161 @@
 import pygame
 import time
 from Snake import Snake
-from Food import Food
-import random
 
 
-check_errors = pygame.init()
+class Game:
+    check_errors = pygame.init()
 
-ASPECT_RATIO = 16/9
+    ASPECT_RATIO = 16/9
 
-HEIGHT = 480
-WIDTH = HEIGHT * ASPECT_RATIO
+    HEIGHT = 480
+    WIDTH = HEIGHT * ASPECT_RATIO
 
-GRID_HEIGHT = 15
-GRID_WIDTH = 15
+    GRID_HEIGHT = 15
+    GRID_WIDTH = 15
 
-SEG_SIZE = min(HEIGHT/GRID_HEIGHT, WIDTH/GRID_WIDTH)
-LEFT_MARGIN = (WIDTH - GRID_WIDTH*SEG_SIZE)/2
-TOP_MARGIN = (HEIGHT - GRID_HEIGHT*SEG_SIZE)/2
+    SEG_SIZE = min(HEIGHT/GRID_HEIGHT, WIDTH/GRID_WIDTH)
+    LEFT_MARGIN = (WIDTH - GRID_WIDTH*SEG_SIZE)/2
+    TOP_MARGIN = (HEIGHT - GRID_HEIGHT*SEG_SIZE)/2
 
+    BODY_GAP = SEG_SIZE*0.03
 
-BODY_GAP = SEG_SIZE*0.03
+    SPEED = 3  # Grid squares per second
+    FPS = 60
 
-SPEED = 3  # Grid squares per second
-FPS = 60
+    snake_color = 0x788374
+    food_color = 0xaa644d
+    text_color = 0xf5e9bf
+    text_bg_color = 0x372a39
+    background_color = 0xf5e9bf
 
-snake_color = 0x788374
-food_color = 0xaa644d
-text_color = 0xf5e9bf
-text_bg_color = 0x372a39
-background_color = 0xf5e9bf
+    def __init__(self) -> None:
+        self.snake = Snake((self.GRID_WIDTH, self.GRID_HEIGHT))
+        self.vision = self.snake.look()
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("SNAKEE")
 
+    def drawBackground(self):
+        self.screen.fill(self.text_bg_color)
+        rect = (self.LEFT_MARGIN, self.TOP_MARGIN, self.WIDTH - 2 *
+                self.LEFT_MARGIN, self.HEIGHT - 2*self.TOP_MARGIN)
+        pygame.draw.rect(self.screen, self.background_color, rect)
 
-def drawBackground():
-    game_window.fill(text_bg_color)
-    rect = (LEFT_MARGIN, TOP_MARGIN, WIDTH - 2 *
-            LEFT_MARGIN, HEIGHT - 2*TOP_MARGIN)
-    pygame.draw.rect(game_window, background_color, rect)
+    def drawSnake(self):
+        for segment in self.snake.body:
+            x, y = self.gridToPixel(segment)
+            x += self.BODY_GAP/2
+            y += self.BODY_GAP/2
 
+            width = self.SEG_SIZE - self.BODY_GAP
+            height = self.SEG_SIZE - self.BODY_GAP
+            rect = (x, y, width, height)
 
-def drawSnake():
-    for segment in snake.body:
-        x = segment[0] * SEG_SIZE + BODY_GAP/2 + LEFT_MARGIN
-        y = segment[1] * SEG_SIZE + BODY_GAP/2 + TOP_MARGIN
-        width = SEG_SIZE - BODY_GAP
-        height = SEG_SIZE - BODY_GAP
+            pygame.draw.rect(self.screen, self.snake_color, rect)
+
+    def drawFood(self):
+        x, y = self.gridToPixel(self.snake.food.pos)
+        x += self.BODY_GAP/2
+        y += self.BODY_GAP/2
+
+        width = self.SEG_SIZE - self.BODY_GAP
+        height = self.SEG_SIZE - self.BODY_GAP
         rect = (x, y, width, height)
-        pygame.draw.rect(game_window, snake_color, rect)
 
+        pygame.draw.rect(self.screen, self.food_color, rect)
 
-def drawFood():
-    x = food.pos[0] * SEG_SIZE + BODY_GAP/2 + LEFT_MARGIN
-    y = food.pos[1] * SEG_SIZE + BODY_GAP/2 + TOP_MARGIN
-    width = SEG_SIZE - BODY_GAP
-    height = SEG_SIZE - BODY_GAP
-    rect = (x, y, width, height)
-    pygame.draw.rect(game_window, food_color, rect)
+    def drawScore(self):
+        font = pygame.font.SysFont("monospace", 72)
+        label = font.render(str(self.snake.size), 1, self.text_color)
+        self.screen.blit(label, (self.WIDTH/2 - label.get_width()/2,
+                                 self.HEIGHT/2 - label.get_height()/2))
 
+    def drawVision(self):
+        for i, distance in enumerate(self.vision):
+            direction = self.snake.directions[i//3]
+            if distance == 0:
+                continue
 
-def renderFrame():
-    drawBackground()
-    drawSnake()
-    drawFood()
+            pos_x = self.snake.body[0][0] + direction[0] * distance
+            pos_y = self.snake.body[0][1] + direction[1] * distance
 
+            offset = [direction[0] * self.SEG_SIZE /
+                      2, direction[1] * self.SEG_SIZE/2]
 
-def getRandFood():
-    x = random.randint(0, GRID_WIDTH-1)
-    y = random.randint(0, GRID_HEIGHT-1)
-    if snake.contains((x, y)):
-        return getRandFood()
-    return Food(x, y)
+            x, y = self.gridToPixel((pos_x, pos_y))
+            x += self.SEG_SIZE/2 - offset[0]
+            y += self.SEG_SIZE/2 - offset[1]
 
+            x2, y2 = self.gridToPixel(self.snake.head)
+            x2 += self.SEG_SIZE/2 + offset[0]
+            y2 += self.SEG_SIZE/2 + offset[1]
 
-def restart():
-    global snake
-    snake = Snake((GRID_WIDTH, GRID_HEIGHT))
+            pygame.draw.circle(self.screen, (255, 0, 0), (x, y), 5)
+            pygame.draw.line(self.screen, (255, 0, 0), (x, y), (x2, y2), 1)
 
+    def gridToPixel(self, grid_pos):
+        x = grid_pos[0] * self.SEG_SIZE + self.LEFT_MARGIN
+        y = grid_pos[1] * self.SEG_SIZE + self.TOP_MARGIN
+        return (x, y)
 
-def gameOver():
-    font = pygame.font.SysFont("monospace", 72)
-    game_window.fill(text_bg_color)
-    label = font.render("GAME OVER", 1, text_color)
-    game_window.blit(label, (WIDTH/2 - label.get_width()/2,
-                             HEIGHT/2 - label.get_height()/2))
-    pygame.display.update()
-    time.sleep(3)
-    restart()
+    def renderFrame(self):
+        self.drawBackground()
+        self.drawSnake()
+        self.drawFood()
+        self.drawVision()
+        pygame.display.update()
 
+    def restart(self):
+        self.snake = Snake((self.GRID_WIDTH, self.GRID_HEIGHT))
 
-fps_controller = pygame.time.Clock()
-fps_controller.tick(FPS)
+    def gameOver(self):
+        font = pygame.font.SysFont("monospace", 72)
+        self.screen.fill(self.text_bg_color)
+        label = font.render("GAME OVER", 1, self.text_color)
+        self.screen.blit(label, (self.WIDTH/2 - label.get_width()/2,
+                                 self.HEIGHT/2 - label.get_height()/2))
+        pygame.display.update()
+        time.sleep(3)
+        self.restart()
 
-game_window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("SNAKEE")
+    def run(self):
+        running = True
 
-running = True
-snake = Snake((GRID_WIDTH, GRID_HEIGHT))
+        score = 0
 
-score = 0
+        start_time = time.time()
+        prev_time = start_time
 
-food = getRandFood()
+        while running:
+            # for loop through the event queue
+            for event in pygame.event.get():
 
-start_time = time.time()
-prev_time = start_time
+                # Check for QUIT event
+                if event.type == pygame.QUIT:
+                    running = False
 
-while running:
-    # for loop through the event queue
-    for event in pygame.event.get():
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.snake.direction = [0, -1]
+                    elif event.key == pygame.K_DOWN:
+                        self.snake.direction = [0, 1]
+                    elif event.key == pygame.K_LEFT:
+                        self.snake.direction = [-1, 0]
+                    elif event.key == pygame.K_RIGHT:
+                        self.snake.direction = [1, 0]
 
-        # Check for QUIT event
-        if event.type == pygame.QUIT:
-            running = False
+            current_time = time.time()
 
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                snake.direction = [0, -1]
-            elif event.key == pygame.K_DOWN:
-                snake.direction = [0, 1]
-            elif event.key == pygame.K_LEFT:
-                snake.direction = [-1, 0]
-            elif event.key == pygame.K_RIGHT:
-                snake.direction = [1, 0]
+            if current_time - prev_time > 1/self.SPEED:
+                prev_time = current_time
+                try:
+                    self.snake.move()
+                    self.vision = self.snake.look()
+                except Exception as e:
+                    print(e)
+                    self.gameOver()
 
-    current_time = time.time()
+                score = len(self.snake.body) * 100
 
-    if current_time - prev_time > 1/SPEED:
-        prev_time = current_time
-        try:
-            snake.move()
-        except Exception as e:
-            print(e)
-            gameOver()
-
-        if snake.head == food.pos:
-            snake.size += 1
-            score += 1
-            food = getRandFood()
-
-    renderFrame()
-    pygame.display.update()
-    fps_controller.tick(FPS)
+            self.renderFrame()
+            pygame.time.Clock().tick(self.FPS)

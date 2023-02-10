8b7c7186a3f79902a34b1f8971d91afddd2809a8
DanielSebasCM <danielsebascm@hotmail.com>
2023-02-10 11:45:53 -0500

    Moved Draw method to the module

--- snake_ml/Food.py
+++ snake_ml/Food.py
@@ -1,6 +1,23 @@
 import numpy as np
+import pygame
+from Config import *
+
 
 class Food:
+
+    color = food_color
+
     def __init__(self, x, y) -> None:
-        self.pos = np.array((x,y))
+        self.pos = np.array((x, y))
         pass
+
+    def draw(self, screen) -> None:
+        x, y = gridToPixel(self.pos)
+        y += FOOD_GAP/2
+        x += FOOD_GAP/2
+
+        width = SEG_SIZE - FOOD_GAP
+        height = SEG_SIZE - FOOD_GAP
+        rect = (x, y, width, height)
+
+        pygame.draw.rect(screen, self.color, rect)
